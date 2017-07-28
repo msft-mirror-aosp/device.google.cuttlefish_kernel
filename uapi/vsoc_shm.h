b812fa686fecfd06e6bdef4d491f7aad07f561c7
Jorge E. Moreira <jemoreira@google.com>
2017-07-27 17:49:55 -0700

    Update kernel to builds 4224550
    Test: TreeHugger
    BUG: 64126984
    Change-Id: I17b48fe271da0d46a7435e53e3cddc077b6b171d

--- uapi/vsoc_shm.h
+++ uapi/vsoc_shm.h
@@ -18,35 +18,45 @@
  ****************************************************************************/
 #ifndef _UAPI_LINUX_VSOC_SHM_H
 #define _UAPI_LINUX_VSOC_SHM_H
+typedef uint32_t vsoc_shm_off_t;
+typedef uint32_t vsoc_reg_off_t;
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+typedef struct {
+  vsoc_reg_off_t begin_offset;
+  vsoc_reg_off_t end_offset;
+  vsoc_reg_off_t owner_offset;
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+  uint32_t owned_value;
+} fd_scoped_permission;
 #define VSOC_REGION_FREE ((uint32_t) 0)
 typedef struct {
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-  uint32_t region_begin_offset;
-  uint32_t region_end_offset;
-  uint32_t owner_offset;
-  uint32_t owned_value;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-} fd_scoped_permission;
+  fd_scoped_permission perm;
+  int32_t managed_region_fd;
+} fd_scoped_permission_arg;
 #define VSOC_NODE_FREE ((uint32_t) 0)
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 typedef struct {
   uint32_t num_nodes_lg2;
+  vsoc_reg_off_t futex_uaddr_table_offset;
+  vsoc_reg_off_t interrupt_signalled_offset;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-  uint32_t futex_uaddr_table_offset;
-  uint32_t interrupt_signalled_offset;
 } vsoc_signal_table_layout;
 typedef char vsoc_device_name[16];
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+#define VSOC_REGION_WHOLE ((int32_t) 0)
 typedef struct {
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
   uint16_t current_version;
   uint16_t min_compatible_version;
-  uint32_t region_begin_offset;
+  vsoc_reg_off_t region_begin_offset;
+  vsoc_reg_off_t region_end_offset;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-  uint32_t region_end_offset;
-  uint32_t offset_of_region_data;
+  vsoc_reg_off_t offset_of_region_data;
   vsoc_signal_table_layout guest_to_host_signal_table;
   vsoc_signal_table_layout host_to_guest_signal_table;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
   vsoc_device_name device_name;
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+  uint32_t managed_by;
 } vsoc_device_region;
 typedef struct {
   uint16_t major_version;
@@ -57,7 +67,7 @@ typedef struct {
   uint32_t vsoc_region_desc_offset;
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 } vsoc_shm_layout_descriptor;
-#define CURRENT_VSOC_LAYOUT_MAJOR_VERSION 1
+#define CURRENT_VSOC_LAYOUT_MAJOR_VERSION 2
 #define CURRENT_VSOC_LAYOUT_MINOR_VERSION 0
 #define VSOC_CREATE_FD_SCOPED_PERMISSION _IOW(0xF5, 0, fd_scoped_permission)
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
