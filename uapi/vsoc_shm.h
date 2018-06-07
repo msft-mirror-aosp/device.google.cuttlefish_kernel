cff931424411eba7b461380a2bdcd5a3ec90dc1f
Greg Hartman <ghartman@google.com>
2018-06-06 19:02:35 -0700

    Update kernel to builds 4825399
    Test: Updated latest pi-dev-plus-aosp, no error dialog
    BUG: 80273334
    Change-Id: I4bebf673150661b729d6add91b04a71393bf58f3

--- uapi/vsoc_shm.h
+++ uapi/vsoc_shm.h
@@ -20,82 +20,66 @@
 #define _UAPI_LINUX_VSOC_SHM_H
 #include <linux/types.h>
 struct fd_scoped_permission {
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
   __u32 begin_offset;
   __u32 end_offset;
   __u32 owner_offset;
   __u32 owned_value;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 };
 #define VSOC_REGION_FREE ((__u32) 0)
 struct fd_scoped_permission_arg {
   struct fd_scoped_permission perm;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
   __s32 managed_region_fd;
 };
 #define VSOC_NODE_FREE ((__u32) 0)
 struct vsoc_signal_table_layout {
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
   __u32 num_nodes_lg2;
   __u32 futex_uaddr_table_offset;
   __u32 interrupt_signalled_offset;
 };
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #define VSOC_REGION_WHOLE ((__s32) 0)
 #define VSOC_DEVICE_NAME_SZ 16
 struct vsoc_device_region {
   __u16 current_version;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
   __u16 min_compatible_version;
   __u32 region_begin_offset;
   __u32 region_end_offset;
   __u32 offset_of_region_data;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
   struct vsoc_signal_table_layout guest_to_host_signal_table;
   struct vsoc_signal_table_layout host_to_guest_signal_table;
   char device_name[VSOC_DEVICE_NAME_SZ];
   __u32 managed_by;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 };
 struct vsoc_shm_layout_descriptor {
   __u16 major_version;
   __u16 minor_version;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
   __u32 size;
   __u32 region_count;
   __u32 vsoc_region_desc_offset;
 };
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #define CURRENT_VSOC_LAYOUT_MAJOR_VERSION 2
 #define CURRENT_VSOC_LAYOUT_MINOR_VERSION 0
 #define VSOC_CREATE_FD_SCOPED_PERMISSION _IOW(0xF5, 0, struct fd_scoped_permission)
 #define VSOC_GET_FD_SCOPED_PERMISSION _IOR(0xF5, 1, struct fd_scoped_permission)
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #define VSOC_MAYBE_SEND_INTERRUPT_TO_HOST _IO(0xF5, 2)
 #define VSOC_WAIT_FOR_INCOMING_INTERRUPT _IO(0xF5, 3)
 #define VSOC_DESCRIBE_REGION _IOR(0xF5, 4, struct vsoc_device_region)
 #define VSOC_SELF_INTERRUPT _IO(0xF5, 5)
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #define VSOC_SEND_INTERRUPT_TO_HOST _IO(0xF5, 6)
 enum wait_types {
   VSOC_WAIT_UNDEFINED = 0,
   VSOC_WAIT_IF_EQUAL = 1,
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
   VSOC_WAIT_IF_EQUAL_TIMEOUT = 2
 };
 struct vsoc_cond_wait {
   __u32 offset;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
   __u32 value;
   __u64 wake_time_sec;
   __u32 wake_time_nsec;
   __u32 wait_type;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
   __u32 wakes;
   __u32 reserved_1;
 };
 #define VSOC_COND_WAIT _IOWR(0xF5, 7, struct vsoc_cond_wait)
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #define VSOC_COND_WAKE _IO(0xF5, 8)
 #endif
 
